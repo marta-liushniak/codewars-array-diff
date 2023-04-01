5d8978a7a5d7fe37e5022fcde8753232bc6a8d3f
Marta Liushniak <marta.liushniak@yahoo.com>
2023-04-01 17:15:00 +0300

    array-diff-solution

--- src/main/java/arraydiff/Main.java
+++ src/main/java/arraydiff/Main.java
@@ -1,7 +1,16 @@
 package arraydiff;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println(Arrays.toString(arrayDiff(new int[]{1, 2, 2, 2, 3}, new int[]{2})));
+    }
 
+    public static int[] arrayDiff(int[] a, int[] b) {
+        return Arrays
+                .stream(a)
+                .filter(x -> Arrays.stream(b).noneMatch(y -> y == x))
+                .toArray();
     }
 }
